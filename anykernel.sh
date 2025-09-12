0253ab8dddafd0e2bca423f90d83652f3edbf382
xx2901318208 <198024072+xx2901318208@users.noreply.github.com>
2025-09-12 11:25:44 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,85 +1,71 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers & GitHub @xx2901318208
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
-do.modules=1
-do.systemless=1
+kernel.string=KernelSU by KernelSU Developers | Build by 1263599071
+do.devicecheck=0
+do.modules=0
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=pineapple
+device.name1=
 device.name2=
 device.name3=
 device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+### AnyKernel install
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
 
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    6.6*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
 
+ui_print "内核构建者: Coolapk@R1263599071"
+ui_print " " "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
-## AnyKernel boot install
-dump_boot;
-
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "cpuctl cpu,timer_slack,none";
-replace_string init.rc "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-replace_string init.tuna.rc "cgroup /dev/cpuctl" "cgroup /dev/cpuctl cpu,timer_slack";
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,barrier=1,discard";
-patch_fstab fstab.tuna /cache ext4 options "nosuid,nodev" "nosuid,nodev,discard";
-patch_fstab f-stab.tuna /data ext4 options "nosuid,nodev,noauto_da_alloc" "nosuid,nodev,noauto_da_alloc,discard";
-
-write_boot;
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
 ## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
-
-# =================================================================
-# 【在这里添加我们的自定义模块加载命令】
-# =================================================================
+# 优先选择模块路径
+if [ -f "$AKHOME/ZRAM-Module.zip" ]; then
+    MODULE_PATH="$AKHOME/ZRAM-Module.zip"
+    KSUD_PATH="/data/adb/ksud"
+    if [ -f "$KSUD_PATH" ]; then
+        ui_print "Installing zram Module..."
+        /data/adb/ksud module install "$MODULE_PATH"
+        ui_print "Installation Complete!"
+    else
+        ui_print "KSUD Not Found, skipping installation..."
+    fi
+else
+    ui_print "ZRAM-Module module Not Found, skipping ZRAM-Module module installation"
+fi
 ui_print " ";
 ui_print ">> Loading custom modules...";
 # 在 init.rc 的 post-fs_data 阶段，插入加载 baseband_guard 模块的命令
